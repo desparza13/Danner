f2796207c78a4d2f3b3d2c4f04d33fc3bbc8ee80
Jennifer Hernández <jennifer.hernandez@iteso.mx>
2023-03-20 22:59:24 -0600

    crear controlador lectores

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 
 const rutaAutores = require('./autores')
+const rutaLectores = require('./lectores')
+
 router.use('/autores',rutaAutores);
+router.use('/lectores',rutaLectores);
 
 module.exports = router;
