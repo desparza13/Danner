f6a961ab99197291d73392a28f0768d91a714c6d
Jennifer Hernández <jennifer.hernandez@iteso.mx>
2023-03-21 20:26:44 -0600

    Creación de controlador de libros, reviews y de solicitudes

--- src/controllers/lectores.js
+++ src/controllers/lectores.js
@@ -38,7 +38,7 @@ const LectoresController={
                 res.status(201).send(lector);
             })
             .catch(error=>{
-                res.status(400).send(error);
+                res.status(400).send('No se pudo crear el lector con usuario ' + lectorNuevo.usuario);
             })
     },
     actualizar:(req,res)=>{
