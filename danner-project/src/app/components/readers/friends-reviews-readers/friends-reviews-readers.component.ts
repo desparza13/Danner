ef843838c9766f0112f849d77a16dbf5179e517a
desparza13 <desparzaespinosa@gmail.com>
2023-04-23 08:53:55 -0600

    Estructura frontend de los reviews, dar estilo y mostrar toda la info, además filtrar a solo reviews de tus amigos

--- danner-project/src/app/components/readers/friends-reviews-readers/friends-reviews-readers.component.ts
+++ danner-project/src/app/components/readers/friends-reviews-readers/friends-reviews-readers.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Review } from 'src/app/shared/interfaces/review';
 import { ReviewService } from 'src/app/shared/services/review.service';
+import { Reader } from 'src/app/shared/interfaces/reader';
+import { ReaderService } from 'src/app/shared/services/reader.service';
 
 @Component({
   selector: 'app-friends-reviews-readers',
@@ -11,18 +13,28 @@ export class FriendsReviewsReadersComponent {
   reader: any;
   readerId: string = '';
   isLoading = true;
-
+  friends: Array<String> = [];
   reviews: Array<Review> = [];
   filterReviews: Array<Review> = [];
-  constructor(private reviewService: ReviewService) {
+  constructor(
+    private reviewService: ReviewService,
+    private readerService: ReaderService) {
 
   }
   ngOnInit() {
     this.reader = JSON.parse(localStorage.getItem('loginUser') || '{}');
     console.log(this.reader);
     this.readerId = this.reader.userId;
-    this.getReviews();
-
+    this.getData();
+  }
+  getData(){
+    this.readerService.getOneReader(this.readerId).subscribe((response:any)=>{
+      this.friends = response.friends.map((friend:Reader) => friend._id);
+      this.getReviews()
+    },
+    (error)=>{
+      this.isLoading = false;
+    });
   }
   getReviews() {
     this.reviewService.getReviews().subscribe((response: any) => {
@@ -34,6 +46,9 @@ export class FriendsReviewsReadersComponent {
   getFilterReviews(){
     this.filterReviews = this.reviews.filter((review)=>{
       return review.userId._id != this.readerId;
+    })
+    this.filterReviews = this.filterReviews.filter((review)=>{
+      return this.friends.includes(review.userId._id)
     })
     this.isLoading=false;
     console.log(this.filterReviews);
