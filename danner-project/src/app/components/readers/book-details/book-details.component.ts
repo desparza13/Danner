e840826020538081ecf6681f9aaa069e63adbc5e
desparza13 <desparzaespinosa@gmail.com>
2023-04-23 17:56:14 -0600

    Añadir funcionalidad a los botones para gestionar las listas

--- danner-project/src/app/components/readers/book-details/book-details.component.ts
+++ danner-project/src/app/components/readers/book-details/book-details.component.ts
@@ -6,7 +6,8 @@ import { ReviewService } from 'src/app/shared/services/review.service';
 import { Reader } from 'src/app/shared/interfaces/reader';
 import { ReaderService } from 'src/app/shared/services/reader.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
-
+import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-book-details',
@@ -44,6 +45,9 @@ export class BookDetailsComponent implements OnInit{
   };
   isLoading = true;
   friends: Array<String> = [];
+  current: Array<String> = [];
+  tbr: Array<String> = [];
+  finished: Array<String> = [];
   reviews: Array<Review> = [];
   filterReviews: Array<Review> = [];
   rating: any;
@@ -52,7 +56,9 @@ export class BookDetailsComponent implements OnInit{
     private bookService: BookService,
     private reviewService: ReviewService,
     private readerService: ReaderService,
-    private snackBar: MatSnackBar,){
+    private snackBar: MatSnackBar,
+    private dialog: MatDialog
+    ){
 
   }
   ngOnInit(){
@@ -64,10 +70,20 @@ export class BookDetailsComponent implements OnInit{
     this.getData();
   }
   getData() {
-    this.reviewService.getReviews().subscribe((response: any) => {
-      this.filterReviews=response;
-      this.getFilterReviews();
-    });
+    this.readerService.getOneReader(this.readerId).subscribe((response:any)=>{
+      this.profile = response;
+      this.friends = response.friends.map((friend:Reader) => friend._id);
+      this.current = response.reading.map((book:any) => book.bookId._id);
+      this.finished = response.read.map((book:any) => book.bookId._id);
+      this.tbr = response.toBeRead.map((book:any) => book._id);
+      this.reviewService.getReviews().subscribe((response: any) => {
+        this.filterReviews=response;
+        this.getFilterReviews();
+      });
+    },
+    (error)=>{
+      this.isLoading = false;
+    })
   }
   getFilterReviews(){
     this.filterReviews = this.filterReviews.filter((review)=>{
@@ -82,6 +98,97 @@ export class BookDetailsComponent implements OnInit{
       return "favorite"
     }
     return "favorite_border"
+  }
+  currentBtn(){
+    if(this.current.includes(this.book._id)){
+      return "Delete from currently reading"
+    }
+    return "Mark as currently reading"
+  }
+  finishedBtn(){
+    if(this.finished.includes(this.book._id)){
+      return "Delete from finished list"
+    }
+    return "Mark as finished"
+  }
+  tbrBtn(){
+    if(this.tbr.includes(this.book._id)){
+      return "Delete from to be read list"
+    }
+    return "Add to to be read list"
+  }
+  modifyCurrentList(){
+    if(this.current.includes(this.book._id)){
+      this.profile.reading = this.profile.reading.filter((book:any) => book.bookId._id !== this.book._id);
+    }else{
+      let readingBook = {
+        "bookId": this.book._id,
+        "progress": 0
+      }
+      this.profile.reading.push(readingBook);
+    }
+    this.readerService.updateReader(this.profile, this.readerId).subscribe(
+      (response: any) => {
+        this.snackBar.open('Currently reading list edited successfully', 'Close', {
+          duration: 3000
+        });
+        console.log("update response",response)
+        this.getData()
+      },
+      (error) => {
+        console.log(error);
+        this.snackBar.open('There was an error editing the currently reading list. Please try again later.', 'Close', {
+          duration: 3000
+        });
+      }
+    );
+  }
+  modifyFinishedList(){
+    if(this.finished.includes(this.book._id)){
+      this.profile.read = this.profile.read.filter((book:any) => book.bookId._id !== this.book._id);
+    }else{
+      let finishedBook = {
+        "bookId": this.book._id,
+        "progress": 0
+      }
+      this.profile.read.push(finishedBook);
+    }
+    this.readerService.updateReader(this.profile, this.readerId).subscribe(
+      (response: any) => {
+        this.snackBar.open('Finished reading list edited successfully', 'Close', {
+          duration: 3000
+        });
+        this.getData()
+      },
+      (error) => {
+        console.log(error);
+        this.snackBar.open('There was an error editing the finished reading list. Please try again later.', 'Close', {
+          duration: 3000
+        });
+      }
+    );
+  }
+  modifyTbrList(){
+    console.log("Tbr",this.tbr)
+    if(this.tbr.includes(this.book._id)){
+      this.profile.toBeRead = this.profile.toBeRead.filter((book:any) => book._id !== this.book._id);
+    }else{
+      this.profile.toBeRead.push(this.book._id);
+    }
+    this.readerService.updateReader(this.profile, this.readerId).subscribe(
+      (response: any) => {
+        this.snackBar.open('To be read list edited successfully', 'Close', {
+          duration: 3000
+        });
+        this.getData()
+      },
+      (error) => {
+        console.log(error);
+        this.snackBar.open('There was an error editing the to be read reading list. Please try again later.', 'Close', {
+          duration: 3000
+        });
+      }
+    );
   }
   likeReview(review:Review){
     let likes = review.likes.map((reader:Reader) => reader._id);
--- danner-project/src/app/shared/interfaces/reader.ts
+++ danner-project/src/app/shared/interfaces/reader.ts
@@ -8,7 +8,7 @@ export interface Reader {
     password: string
     read: Array<object>,
     toBeRead: Array<string>,
-    reading:Array<string>,
+    reading:Array<object>,
     friends: Array<string>,
     __v?:number,
     readingChallenge: number
