137da8333087740ae4f361e2db5d0cc78ee9012a
desparza13 <desparzaespinosa@gmail.com>
2023-04-23 09:16:58 -0600

    los lectores pueden dar me gustas y eliminarlos

--- danner-project/src/app/components/readers/friends-reviews-readers/friends-reviews-readers.component.ts
+++ danner-project/src/app/components/readers/friends-reviews-readers/friends-reviews-readers.component.ts
@@ -3,6 +3,8 @@ import { Review } from 'src/app/shared/interfaces/review';
 import { ReviewService } from 'src/app/shared/services/review.service';
 import { Reader } from 'src/app/shared/interfaces/reader';
 import { ReaderService } from 'src/app/shared/services/reader.service';
+import { response } from 'express';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-friends-reviews-readers',
@@ -12,13 +14,29 @@ import { ReaderService } from 'src/app/shared/services/reader.service';
 export class FriendsReviewsReadersComponent {
   reader: any;
   readerId: string = '';
+  profile: Reader = {
+    _id: "",
+    name: "",
+    user: '',
+    email: '',
+    city: '',
+    image: '',
+    password: '',
+    read: [],
+    toBeRead: [],
+    reading: [],
+    friends: [],
+    readingChallenge: 0
+  };
   isLoading = true;
   friends: Array<String> = [];
   reviews: Array<Review> = [];
   filterReviews: Array<Review> = [];
   constructor(
     private reviewService: ReviewService,
-    private readerService: ReaderService) {
+    private readerService: ReaderService,
+    private snackBar: MatSnackBar,
+    ) {
 
   }
   ngOnInit() {
@@ -29,6 +47,7 @@ export class FriendsReviewsReadersComponent {
   }
   getData(){
     this.readerService.getOneReader(this.readerId).subscribe((response:any)=>{
+      this.profile = response;
       this.friends = response.friends.map((friend:Reader) => friend._id);
       this.getReviews()
     },
@@ -52,5 +71,31 @@ export class FriendsReviewsReadersComponent {
     })
     this.isLoading=false;
     console.log(this.filterReviews);
+  }
+  likeIcon(review:Review){
+    let likes = review.likes.map((reader:Reader) => reader._id);
+    if(likes.includes(this.readerId)){
+      return "favorite"
+    }
+    return "favorite_border"
+  }
+  likeReview(review:Review){
+    let likes = review.likes.map((reader:Reader) => reader._id);
+    if(likes.includes(this.readerId)){
+      review.likes = review.likes.filter((reader:any) => reader._id !== this.readerId);
+    }else{
+      review.likes.push(this.profile);
+    }
+    this.reviewService.updateReview(review, review._id).subscribe(
+      (response: any) => {
+        review = response;
+      },
+      (error)=>{
+        console.log(error);
+        this.snackBar.open('There was an error. Please try again later.', 'Close', {
+          duration: 3000
+        });
+      }
+    )
   }
 }
