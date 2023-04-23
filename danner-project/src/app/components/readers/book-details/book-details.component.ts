5ae631f0c92b743386b33fe5823cc18e71de2c17
desparza13 <desparzaespinosa@gmail.com>
2023-04-23 17:12:39 -0600

    Mostrar reviews del libro

--- danner-project/src/app/components/readers/book-details/book-details.component.ts
+++ danner-project/src/app/components/readers/book-details/book-details.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from 'src/app/shared/interfaces/book';
 import { BookService } from 'src/app/shared/services/book.service';
+import { Review } from 'src/app/shared/interfaces/review';
+import { ReviewService } from 'src/app/shared/services/review.service';
+import { Reader } from 'src/app/shared/interfaces/reader';
+import { ReaderService } from 'src/app/shared/services/reader.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-book-details',
@@ -20,12 +26,80 @@ export class BookDetailsComponent implements OnInit{
     pages: 0,
     showDescription: false
   }
+  reader: any;
+  readerId: string = '';
+  profile: Reader = {
+    _id: "",
+    name: "",
+    user: '',
+    email: '',
+    city: '',
+    image: '',
+    password: '',
+    read: [],
+    toBeRead: [],
+    reading: [],
+    friends: [],
+    readingChallenge: 0
+  };
+  isLoading = true;
+  friends: Array<String> = [];
+  reviews: Array<Review> = [];
+  filterReviews: Array<Review> = [];
+  rating: any;
 
-  constructor(private bookService: BookService){
+  constructor(
+    private bookService: BookService,
+    private reviewService: ReviewService,
+    private readerService: ReaderService,
+    private snackBar: MatSnackBar,){
 
   }
   ngOnInit(){
     this.book = this.bookService.getBook()
     console.log(this.book);
+    this.reader = JSON.parse(localStorage.getItem('loginUser') || '{}');
+    console.log(this.reader);
+    this.readerId = this.reader.userId;
+    this.getData();
+  }
+  getData() {
+    this.reviewService.getReviews().subscribe((response: any) => {
+      this.filterReviews=response;
+      this.getFilterReviews();
+    });
+  }
+  getFilterReviews(){
+    this.filterReviews = this.filterReviews.filter((review)=>{
+      return review.bookId._id == this.book._id
+    })
+    this.isLoading=false;
+    console.log(this.filterReviews);
+  }
+  likeIcon(review:Review){
+    let likes = review.likes.map((reader:Reader) => reader._id);
+    if(likes.includes(this.readerId)){
+      return "favorite"
+    }
+    return "favorite_border"
+  }
+  likeReview(review:Review){
+    let likes = review.likes.map((reader:Reader) => reader._id);
+    if(likes.includes(this.readerId)){
+      review.likes = review.likes.filter((reader:any) => reader._id !== this.readerId);
+    }else{
+      review.likes.push(this.profile);
+    }
+    this.reviewService.updateReview(review, review._id).subscribe(
+      (response: any) => {
+        review = response;
+      },
+      (error)=>{
+        console.log(error);
+        this.snackBar.open('There was an error. Please try again later.', 'Close', {
+          duration: 3000
+        });
+      }
+    )
   }
 }
