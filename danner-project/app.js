ded7f57a4d84254e2e63023057c16e551c7242ee
desparza13 <desparzaespinosa@gmail.com>
2023-05-13 23:13:31 -0600

    app.js para host

---
+++ danner-project/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist', 'danner-project')));
+
+app.get('', (req, res) => {
+    const url = path.join(__dirname, 'dist', 'danner-project', 'index.html');
+    res.sendFile(url);
+});
+
+app.listen(5000, () => {
+    console.log('app is running');
+})
